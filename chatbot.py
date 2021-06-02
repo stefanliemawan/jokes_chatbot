4ce59b96b882925562ffc1e1d2fa67b4d936de0b
stefanliemawan <stefanliemawan@gmail.com>
2021-06-02 22:53:40 +0100

    stats

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,43 @@
+from numpy.random import randint
 import pandas as pd
 
 pd.set_option('display.max_colwidth', None)
 
-data = pd.read_csv("./dataset/wocka-stupidstuff.csv").reset_index(drop=True)
+jokes = pd.read_csv("./dataset/wocka-stupidstuff.csv",
+                    escapechar='\\').reset_index(drop=True)
+
+stats = pd.read_csv("./dataset/ws-stats.csv").reset_index(drop=True)
+# stats = pd.read_csv("./stats/user1.csv").reset_index(drop=True)
+
+# make a formula to get random jokes based on stats
+
+while True:
+    print("Jokes? (y/n)")
+    ans = input()
+    rand = randint(len(jokes))
+    if ans == "y":
+        joke = jokes.loc[rand]["body"]
+        category = jokes.loc[rand]["category"]
+        print("\n--------------------------------------------------------\n")
+        print(joke)
+        print("\n--------------------------------------------------------\n")
+
+        print("How's the joke? (hilarious/funny/normal/bad/horrible)")
+        rev = input()
+        stats.loc[stats["category"] == category, rev] += 1
+
+        if rev == "hilarous":
+            print("\nAlways knew you have a good sense of humour\n")
+        if rev == "funny":
+            print("\nLOL LMAO\n")
+        if rev == "normal":
+            print("\nAlrighty then!\n")
+        if rev == "bad":
+            print("\nAh, you're just shit\n")
+        if rev == "horrible":
+            print("\nHAHAHAHAHA (laughing sarcastically)\n")
+
+    elif ans == "n":
+        break
+
+stats.to_csv("./stats/user1.csv", index=False)
---
+++ init_dataset.py
@@ -0,0 +1,51 @@
+import pandas as pd
+from sklearn.utils import shuffle
+
+wocka = pd.read_json("./joke-dataset-master/wocka.json")
+wocka.drop(["id", "title"], axis=1, inplace=True)
+# print(wocka)
+
+stupidstuff = pd.read_json("./joke-dataset-master/stupidstuff.json")
+stupidstuff.drop(["id", "rating"], axis=1, inplace=True)
+# print(stupidstuff)
+
+data = pd.concat([wocka, stupidstuff]).reset_index(drop=True)
+
+data["category"].replace("Animals", "Animal", inplace=True)
+data["category"].replace("Bar Jokes", "Bar", inplace=True)
+data["category"].replace("Blind Jokes", "Blind", inplace=True)
+data["category"].replace("Blonde Jokes", "Blonde", inplace=True)
+data["category"].replace("Blond", "Blonde", inplace=True)
+data["category"].replace("Computer", "Computer", inplace=True)
+data["category"].replace("Crazy Jokes", "Crazy", inplace=True)
+data["category"].replace("Ethnic Jokes", "Ethnic", inplace=True)
+data["category"].replace("Farmers", "Farmer", inplace=True)
+data["category"].replace("Food Jokes", "Food", inplace=True)
+data["category"].replace("Holidays", "Holiday", inplace=True)
+data["category"].replace("Idiots", "Idiot", inplace=True)
+data["category"].replace("Insults", "Insult", inplace=True)
+data["category"].replace("Lawyers", "Lawyer", inplace=True)
+data["category"].replace("Light Bulbs", "Light Bulb", inplace=True)
+data["category"].replace("Lightbulb", "Light Bulb", inplace=True)
+data["category"].replace("Office Jokes", "Office", inplace=True)
+data["category"].replace("One Liners", "One Liner", inplace=True)
+data["category"].replace("Other / Misc", "Miscellaneous", inplace=True)
+data["category"].replace("Police Jokes", "Police", inplace=True)
+data["category"].replace("Puns", "Pun", inplace=True)
+data["category"].replace("Sports", "Sport", inplace=True)
+data["category"].replace("State Jokes", "State", inplace=True)
+data["category"].replace("Yo Mama", "Yo Momma", inplace=True)
+
+data.dropna(inplace=True)
+data = shuffle(data)
+data.to_csv("./dataset/wocka-stupidstuff.csv", encoding="utf-8", index=False)
+print(data)
+
+# categories = data["category"].unique()
+# categories = pd.Series(categories)
+stats = pd.DataFrame(
+    {"category": data["category"], "hilarious": 0, "funny": 0, "normal": 0, "bad":0,"horrible": 0})
+stats.drop_duplicates("category", inplace=True)
+stats.sort_values("category", inplace=True)
+stats.to_csv("./dataset/ws-stats.csv", index=False)
+print(stats)
