730e652e019d8e15d55f1d826461e6787259428b
stefanliemawan <stefanliemawan@gmail.com>
2021-06-07 22:21:56 +0100

    fixed stuff, tidy up

--- chatbot.py
+++ chatbot.py
@@ -5,14 +5,16 @@ import os
 
 # pd.set_option('display.max_colwidth', None)
 
+# keyword?
+
 
 users = os.listdir("./stats")
 print("Users found", users)
 
-print("Type in a username")
-user = input()
+# print("Type in a username")
+# user = input()
+user = "test"
 
-print("\nHello,", user, "\n")
 if user not in users:
     os.mkdir(f"./stats/{user}")
 
@@ -27,69 +29,105 @@ except:
                         escapechar='\\').reset_index(drop=True)
     stats = pd.read_csv("./dataset/ws-stats.csv").reset_index(drop=True)
 
+# read_csv have some nan value even though the dataset does not from init_dataset
+jokes.dropna(inplace=True)
 
-def calculatePercentage():
+
+def calculatePercentage(stats):
     n = stats.hilarious + stats.funny + stats.normal + stats.bad + stats.horrible
     n[n == 0] = 1
     score = 1 * stats.hilarious + 0.8 * stats.funny + 0.6 * \
         stats.normal + 0.4 * stats.bad + 0.2 * stats.horrible / n
     score[score == 0] = 1/len(score)
     stats["score"] = score
+    return stats
 
 
-calculatePercentage()
+def returnJokeBasedOnCategory(jokes, category):
+    chosen = jokes[jokes.category == category]
+    rand = randint(len(chosen))
+    joke = chosen.iloc[rand]["body"]
+    return joke
+
+
+def handleFeedback(stats, category):
+    print("How's the joke? (hilarious/funny/normal/bad/horrible)")
+    rev = input()
+
+    if rev == "hilarious":
+        print("\nAlways knew you have a good sense of humour\n")
+        stats.loc[stats["category"] == category, rev] += 1
+    elif rev == "funny":
+        print("\nLOL LMAO\n")
+        stats.loc[stats["category"] == category, rev] += 1
+    elif rev == "normal":
+        print("\nAlrighty then!\n")
+        stats.loc[stats["category"] == category, rev] += 1
+    elif rev == "bad":
+        print("\nAh, you're just shit\n")
+        stats.loc[stats["category"] == category, rev] += 1
+    elif rev == "horrible":
+        print("\nHAHAHAHAHA (laughing sarcastically)\n")
+        stats.loc[stats["category"] == category, rev] += 1
+    else:
+        print(
+            "\nWhat's that? I see... \nYou don't like personalized stuff, do you?\nVery well, then\n")
+    return stats
+
+
+def handleJoke(jokes, joke):
+    print("\n--------------------------------------------------------\n")
+    print(joke)
+    print("\n--------------------------------------------------------\n")
+
+    jokes = jokes[jokes.body != joke]
+    print(len(jokes), "Jokes remaining\n")
+    return jokes
+
+
+stats = calculatePercentage(stats)
+
+
+print("\nHello,", user, "\n")
+print("Welcome to blahblah chatbot")
+
+categories = stats["category"]
+bodies = jokes["body"]
 
 while True:
-    print("Jokes? (y/n)")
-    ans = input()
-    if ans == "y":
-        categories = stats["category"]
-        print(stats["score"])
-        print(stats["score"].sum())
+    print("What can I do for you?")
+    ans = input().title()
+    if ans == "Joke":
         probs = stats["score"]/stats["score"].sum()
-        print(categories)
-        print(probs)
         category = choices(categories, probs)[0]
-        print(category)
         print("\nChosen category -->", category)
-
-        chosen = jokes[jokes.category == category]
-        rand = randint(len(chosen))
-        joke = jokes.loc[rand]["body"]
-        print("\n--------------------------------------------------------\n")
-        print(joke)
-        print("\n--------------------------------------------------------\n")
-
-        jokes = jokes[jokes.body != joke]
-        print(len(jokes), "Jokes remaining\n")
-
-        print("How's the joke? (hilarious/funny/normal/bad/horrible)")
-        rev = input()
-
-        if rev == "hilarious":
-            print("\nAlways knew you have a good sense of humour\n")
-            stats.loc[stats["category"] == category, rev] += 1
-        elif rev == "funny":
-            print("\nLOL LMAO\n")
-            stats.loc[stats["category"] == category, rev] += 1
-        elif rev == "normal":
-            print("\nAlrighty then!\n")
-            stats.loc[stats["category"] == category, rev] += 1
-        elif rev == "bad":
-            print("\nAh, you're just shit\n")
-            stats.loc[stats["category"] == category, rev] += 1
-        elif rev == "horrible":
-            print("\nHAHAHAHAHA (laughing sarcastically)\n")
-            stats.loc[stats["category"] == category, rev] += 1
+        joke = returnJokeBasedOnCategory(jokes, category)
+        jokes = handleJoke(jokes, joke)
+        stats = handleFeedback(stats, category)
+    elif categories.str.contains(ans).any():
+        category = ans
+        print("\nChosen category -->", category)
+        joke = returnJokeBasedOnCategory(jokes, category)
+        jokes = handleJoke(jokes, joke)
+        stats = handleFeedback(stats, category)
+    elif ans.startswith("Search"):
+        keyword = ans.split()[1].lower()
+        if bodies.str.contains(keyword).any():
+            chosen = jokes[bodies.str.contains(keyword)]
+            rand = randint(len(chosen))
+            joke = chosen.iloc[rand]["body"]
+            category = chosen.iloc[rand]["category"]
+            jokes = handleJoke(jokes, joke)
+            stats = handleFeedback(stats, category)
         else:
-            print(
-                "\nWhat's that? I see... \nYou don't like personalized stuff, do you?\nVery well, then\n")
-
-    elif ans == "n":
+            print("\nKeyword not found\n")
+    elif ans == "Exit":
+        # print("\nWhat do you want from me then?\n")
+        print("Boo\n")
         break
 
 
-calculatePercentage()
+stats = calculatePercentage(stats)
 
 print("Saving your jokes preferences...")
 jokes.to_csv(f"./stats/{user}/{user}-remaining-jokes.csv", index=False)
